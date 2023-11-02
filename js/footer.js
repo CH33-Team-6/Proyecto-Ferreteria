fbfb9a270459ef624f64bbef021444d2a61a6928
Veronika <veronika.s.diaz@gmail.com>
2023-11-01 20:06:53 -0600

    Creación del footer en js

---
+++ js/footer.js
@@ -0,0 +1,23 @@
+ 
+    const footer = document.getElementById("footer");
+    footer.insertAdjacentHTML("afterbegin", `
+    <footer>
+    <div class="cardfooter"> 
+      <h5 class="card-title">Nuestras Sucursales</h5>
+      <p class="card-text">San Antonio 503,<br> 67275 Juárez, N.L.</p>
+      <p class="card-text">Higueras 100, <br>Residencial Los Huertos,<br> 67265 Juárez, N.L.</p>
+    </div>
+    
+    <div class="cardfooter">
+      <h5 class="card-title">Atención a clientes</h5>
+      <p class="card-text">Lunes a Viernes: 8:00a.m.-8:00p.m.<br>
+        Sábado: 8:00p.m.-5:00p.m.</p>
+        <i class="fa-regular fa-envelope fa-lg" style="color: #ffffff;;"></i>
+        <i class="fa-brands fa-facebook fa-lg" style="color: #ffffff;"></i>
+        <i class="fa-brands fa-instagram fa-lg" style="color: #ffffff;"></i>
+        
+      </div>
+    </footer>
+
+`)
+    
