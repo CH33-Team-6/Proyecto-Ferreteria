96d790fda5a52862b19b8794417f53b5d2be6d95
Lilia <lilijusatre@gmail.com>
2023-11-07 20:29:22 -0600

    Correcciones en pantalla móvil para que sea responsivo

--- js/productos.js
+++ js/productos.js
@@ -1,10 +1,10 @@
 function addItem(item){
-    const itemHTML = `<div class="card" style="width: 18rem; object-fit:contain; height:100%; padding:1rem; margin-bottom: 12px;"onmouseover="this.style.backgroundColor='#D9D9D9'"onmouseout="this.style.backgroundColor='transparent'"> 
+    const itemHTML = `<div class="card" style="width: 18rem; object-fit:contain; height:100%; padding:1rem; margin-bottom: 12px; border-radius: 50px;"onmouseover="this.style.backgroundColor='#D9D9D9'"onmouseout="this.style.backgroundColor='transparent'"> 
            <img src= " ${item.img}  " class="card-img-top" alt="image">
            <div class="card-body" style="text-align:center; "> 
-               <h5 class="card-title" style="color:#C10F19;">$${item.precio}</h5>
-              <p class="card-text">${item.nombre}.00</p>
-                <a href="#" class="btn btn-primary">Agregar al carrito</a>
+               <h5 class="card-title" style="color:#C10F19;">$${item.precio}.00</h5>
+              <p class="card-text">${item.nombre}</p>
+                <a href="#" class="btn" style="background-color:#3135A0; color:#fff">Agregar al carrito</a>
             </div>
         </div>
         <br/>`;
