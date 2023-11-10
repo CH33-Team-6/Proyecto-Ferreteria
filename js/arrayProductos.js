9b4e8775b1306f4fb32b95f250672b2dd64bf861
Lilia <lilijusatre@gmail.com>
2023-11-09 18:01:05 -0600

    se agrego arrayProducts.js

---
+++ js/arrayProductos.js
@@ -0,0 +1,72 @@
+export const PRODUCTOS = [
+    {
+      "id": 459,
+      "nombre": "RUGO MAXIFLEX",
+      "img": "./assets/img-productos/PRODUCTOS/459.png",
+      "precio": 55,
+      "descripción": "MANGUERA PARA FREGADERO Y LAVABO 13 X 13 X 55"
+    },
+    {
+      "id": 789,
+      "nombre": "DIABLO HEXAGONAL",
+      "img": "./assets/img-productos/PRODUCTOS/789.png",
+      "precio": 40,
+      "descripción": "DIABLO HEXAGONAL 10"
+    },
+    {
+      "id": 808,
+      "nombre": "COLD PATCH",
+      "img": "./assets/img-productos/PRODUCTOS/808.png",
+      "precio": 40,
+      "descripción": "COLD PATCH C48"
+    },
+    {
+      "id": 809,
+      "nombre": "VERMAR TUBO",
+      "img": "./assets/img-productos/PRODUCTOS/809.png",
+      "precio": 22,
+      "descripción": "TUBO PARCHE PARA BICICLETA 15 ML"
+    },
+    {
+      "id": 819,
+      "nombre": "GVSA CÁMARA R-20",
+      "img": "./assets/img-productos/PRODUCTOS/819.png",
+      "precio": 55,
+      "descripción": "GVSA CÁMARA R-20 X 2.125"
+    },
+    {
+      "id": 820,
+      "nombre": "GVSA CÁMARA R-16",
+      "img": "./assets/img-productos/PRODUCTOS/820.png",
+      "precio": 50,
+      "descripción": "GVSA CÁMARA R-16 X 2.125"
+    },
+    {
+      "id": 827,
+      "nombre": "ALTREGO CADENA STANDAR",
+      "img": "./assets/img-productos/PRODUCTOS/827.png",
+      "precio": 55,
+      "descripción": "ALTREGO CADENA STANDAR 12 X 18 X 114L"
+    },
+    {
+      "id": 900,
+      "nombre": "PENS SELLADOR NEGRO",
+      "img": "./assets/img-productos/PRODUCTOS/900.png",
+      "precio": 80,
+      "descripción": "PENS SELLADOR NEGRO 300 ML"
+    },
+    {
+      "id": 977,
+      "nombre": "CÁMARA PARA MOTO",
+      "img": "./assets/img-productos/PRODUCTOS/977.png",
+      "precio": 70,
+      "descripción": "CÁMARA PARA MOTO PMAYA 183041"
+    },
+    {
+      "id": 986,
+      "nombre": "RUBBER SOLUTION",
+      "img": "./assets/img-productos/PRODUCTOS/986.png",
+      "precio": 15,
+      "descripción": "RUBBER SOLUTION 20 ML"
+    }
+  ]
