5a3a3ab94639e4d8753ebe0a701a3b7f56005235
Gab <gabriela.valderramag@gmail.com>
2023-11-17 18:51:04 -0600

    Se agrega validación de contraseña y usuario

--- js/login.js
+++ js/login.js
@@ -1,4 +1,21 @@
-const correoElement=document.getElementById("exampleInputEmail1");
-const contraseñaElement=document.getElementById("exampleInputPassword1");
-const btnEnviar=document.getElementById("btnEnviar");
 
+
+btnEnviar. addEventListener("click", function(event){
+    const correoElement=document.getElementById("exampleInputEmail1").value;
+    const contraseñaElement=document.getElementById("exampleInputPassword1").value;
+    const btnEnviar=document.getElementById("btnEnviar"); 
+    const errorMessage = document.getElementById("errorMessage");
+
+    if (correoElement === "" || contraseñaElement === "") {
+        errorMessage.txtContent = "Por favor, ingrese nombre de usuario y contraseña";
+    } else {
+        const storedUsarname = localStorage.getItem("correoElement");
+        const storedPassword = localStorage.getItem("contraseñaElement");
+
+        if (correoElement === storedUsarname && contraseñaElement === storedPassword) {
+            errorMessage.txtContent = "Inicio de sesión exitoso.";
+        } else {
+            errorMessage.txtContent = "Nombre de usuario y contraseña incorrectos.";
+        }
+    }
+});//btnEnviar
