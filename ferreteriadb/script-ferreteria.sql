41ec20e92444d7cfcd0f482acb69ecc0668c4568
Ana Mia <ruizmia1@gmail.com>
2023-11-23 18:14:15 -0600

    Introduccion de base de datos
    Se realizo la base de datos del proyecto de ferreteria
    
    Signed-off-by: Ana Mia <“ruizmia1@gmail.com”>

---
+++ ferreteriadb/script-ferreteria.sql
@@ -0,0 +1,138 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema preciojustodb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema preciojustodb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `preciojustodb` DEFAULT CHARACTER SET utf8 ;
+USE `preciojustodb` ;
+
+-- -----------------------------------------------------
+-- Table `preciojustodb`.`carrito-compras`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `preciojustodb`.`carrito-compras` (
+  `idcarrito-compras` INT NOT NULL AUTO_INCREMENT,
+  `fecha-creacion` DATE NOT NULL,
+  `cantidad-productos` INT NOT NULL,
+  `precio-total` DECIMAL(10,0) NOT NULL,
+  PRIMARY KEY (`idcarrito-compras`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `preciojustodb`.`categorias`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `preciojustodb`.`categorias` (
+  `idcategorias` INT NOT NULL AUTO_INCREMENT,
+  `categoria` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`idcategorias`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 7
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `preciojustodb`.`sucursal`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `preciojustodb`.`sucursal` (
+  `idsucursal` INT NOT NULL,
+  `sucursal` VARCHAR(45) NOT NULL,
+  `ubicacion` VARCHAR(100) NOT NULL,
+  `telefono` VARCHAR(15) NOT NULL,
+  PRIMARY KEY (`idsucursal`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `preciojustodb`.`usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `preciojustodb`.`usuario` (
+  `idusuario` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  `numero` INT NOT NULL,
+  `correo electronico` VARCHAR(45) NOT NULL,
+  `carrito-compras_idcarrito-compras` INT NOT NULL,
+  PRIMARY KEY (`idusuario`, `carrito-compras_idcarrito-compras`),
+  INDEX `fk_usuario_carrito-compras1_idx` (`carrito-compras_idcarrito-compras` ASC) VISIBLE,
+  CONSTRAINT `fk_usuario_carrito-compras1`
+    FOREIGN KEY (`carrito-compras_idcarrito-compras`)
+    REFERENCES `preciojustodb`.`carrito-compras` (`idcarrito-compras`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `preciojustodb`.`orden-de-compra`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `preciojustodb`.`orden-de-compra` (
+  `idorden-de-compra` INT NOT NULL AUTO_INCREMENT,
+  `fecha orden` DATE NOT NULL,
+  `status` VARCHAR(45) NOT NULL,
+  `precio-total` DECIMAL(10,0) NOT NULL,
+  `cantidad-productos` INT NOT NULL,
+  `usuario_idusuario` INT NOT NULL,
+  `sucursal_idsucursal` INT NOT NULL,
+  PRIMARY KEY (`idorden-de-compra`, `usuario_idusuario`, `sucursal_idsucursal`),
+  INDEX `fk_orden-de-compra_usuario1_idx` (`usuario_idusuario` ASC) VISIBLE,
+  INDEX `fk_orden-de-compra_sucursal1_idx` (`sucursal_idsucursal` ASC) VISIBLE,
+  CONSTRAINT `fk_orden-de-compra_sucursal1`
+    FOREIGN KEY (`sucursal_idsucursal`)
+    REFERENCES `preciojustodb`.`sucursal` (`idsucursal`),
+  CONSTRAINT `fk_orden-de-compra_usuario1`
+    FOREIGN KEY (`usuario_idusuario`)
+    REFERENCES `preciojustodb`.`usuario` (`idusuario`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `preciojustodb`.`productos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `preciojustodb`.`productos` (
+  `idproductos` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  `imagen` VARCHAR(100) NOT NULL,
+  `precio` DECIMAL(10,0) NOT NULL,
+  `descripcion` VARCHAR(300) NOT NULL,
+  `cantidad` INT NOT NULL,
+  `categorias_idcategorias` INT NOT NULL,
+  `sucursal_idsucursal` INT NOT NULL,
+  `carrito-compras_idcarrito-compras` INT NOT NULL,
+  `orden-de-compra_idorden-de-compra` INT NOT NULL,
+  `orden-de-compra_usuario_idusuario` INT NOT NULL,
+  `orden-de-compra_sucursal_idsucursal` INT NOT NULL,
+  PRIMARY KEY (`idproductos`, `categorias_idcategorias`, `sucursal_idsucursal`, `carrito-compras_idcarrito-compras`, `orden-de-compra_idorden-de-compra`, `orden-de-compra_usuario_idusuario`, `orden-de-compra_sucursal_idsucursal`),
+  INDEX `fk_productos_categorias_idx` (`categorias_idcategorias` ASC) VISIBLE,
+  INDEX `fk_productos_sucursal1_idx` (`sucursal_idsucursal` ASC) VISIBLE,
+  INDEX `fk_productos_carrito-compras1_idx` (`carrito-compras_idcarrito-compras` ASC) VISIBLE,
+  INDEX `fk_productos_orden-de-compra1_idx` (`orden-de-compra_idorden-de-compra` ASC, `orden-de-compra_usuario_idusuario` ASC, `orden-de-compra_sucursal_idsucursal` ASC) VISIBLE,
+  CONSTRAINT `fk_productos_carrito-compras1`
+    FOREIGN KEY (`carrito-compras_idcarrito-compras`)
+    REFERENCES `preciojustodb`.`carrito-compras` (`idcarrito-compras`),
+  CONSTRAINT `fk_productos_categorias`
+    FOREIGN KEY (`categorias_idcategorias`)
+    REFERENCES `preciojustodb`.`categorias` (`idcategorias`),
+  CONSTRAINT `fk_productos_sucursal1`
+    FOREIGN KEY (`sucursal_idsucursal`)
+    REFERENCES `preciojustodb`.`sucursal` (`idsucursal`),
+  CONSTRAINT `fk_productos_orden-de-compra1`
+    FOREIGN KEY (`orden-de-compra_idorden-de-compra` , `orden-de-compra_usuario_idusuario` , `orden-de-compra_sucursal_idsucursal`)
+    REFERENCES `preciojustodb`.`orden-de-compra` (`idorden-de-compra` , `usuario_idusuario` , `sucursal_idsucursal`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
