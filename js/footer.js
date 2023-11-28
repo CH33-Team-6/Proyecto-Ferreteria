9af59fab0eca402965ff4a555272f5d6a83ef1a0
Lilia <lilijusatre@gmail.com>
2023-11-28 11:07:35 -0600

    cambios en el footer

--- js/footer.js
+++ js/footer.js
@@ -1,25 +1,22 @@
- 
-    const footer = document.getElementById("footer");
-    footer.insertAdjacentHTML("afterbegin", `
-    <footer>
+const footer = document.getElementById('footer');
+footer.insertAdjacentHTML(
+  'afterbegin',
+  `
     <div class="cardfooter"> 
       <h4 class="card-title">Nuestras Sucursales</h4><hr>
       <p class="card-text">San Antonio 503,<br> 67275 Juárez, N.L.</p>
       <p class="card-text">Higueras 100, <br>Residencial Los Huertos,<br> 67265 Juárez, N.L.</p>
     </div>
-    
     <div class="cardfooter">
       <h4 class="card-title">Atención a clientes</h4><hr>
       <p class="card-text">Lunes a Viernes:</br> 8:00a.m.-8:00p.m.<br>
         Sábado:</br> 8:00p.m.-5:00p.m.</p>
-      </div>
-      <div class = "cardfooter">
+    </div>
+    <div class = "cardfooter">
       <h4 class="card-title redes-links">Redes Sociales</h4><hr>
       <i class="fa-regular fa-envelope fa-xl redes-link" style="color: #ffffff;;"></i>
       <i class="fa-brands fa-facebook fa-xl redes-link" style="color: #ffffff;"></i>
       <i class="fa-brands fa-instagram fa-xl redes-link" style="color: #ffffff;"></i>
-      </div>
-    </footer>
-
-`);
-    
+    </div>
+    `,
+);
